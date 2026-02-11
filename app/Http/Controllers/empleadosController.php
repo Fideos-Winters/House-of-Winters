0dc66c5861c9ad0aa3a85be14eec05ffae4f189f
Your Name <you@example.com>
2026-02-11 11:42:37 -0600

    validacion de imagen en apartado de empleado

--- app/Http/Controllers/empleadosController.php
+++ app/Http/Controllers/empleadosController.php
@@ -12,83 +12,89 @@ class empleadosController extends Controller
      */
     public function index()
     {
-    
-        $empl = empleados:: all();//select * from empleados
-        //return $empl;
-        return view('/equipo/listado')-> with('empleados', $empl);
-    
+
+        $empl = empleados::all(); // select * from empleados
+
+        // return $empl;
+        return view('/equipo/listado')->with('empleados', $empl);
+
     }
 
     public function create()
     {
 
-    return view('/Equipo/aEquipo');
-
+        return view('/Equipo/aEquipo');
 
     }
 
     public function store(Request $req)
     {
 
-    //return $req -> all();
-    
-    $empld = new empleados();
+        // return $req -> all();
 
-    $empld -> Nombres = $req -> Nombres;
-    $empld -> Apellidos = $req -> Apellidos;
-    $empld -> Correo = $req -> Correo;
-    $empld -> Contrasena = $req -> Contrasena;
-    $empld -> Rol = $req -> Rol;
-    $empld -> Imagen = $req -> Imagen;
+        $empld = new empleados;
 
-    $empld -> save();
+        $empld->Nombres = $req->Nombres;
+        $empld->Apellidos = $req->Apellidos;
+        $empld->Correo = $req->Correo;
+        $empld->Contrasena = $req->Contrasena;
+        $empld->Rol = $req->Rol;
+        $empld->Imagen = '/imagenes/empleados/Empleado_default.jpg';
 
-    return redirect('/equipo/listado');
+        $empld->save();
 
+        if ($req->hasFile('Imagen')) {
+            $imagen = $req->file('Imagen');
+            $nuevo_nombre = 'Empleado_'.$empld->ID.'.jpg';
+            $ruta = $imagen->storeAs('Imagenes/Empleados', $nuevo_nombre, 'public');
+            $empld->Imagen = '/storage/'.$ruta;
+            $empld->save();
+        }
+
+        return redirect('/equipo/listado');
 
     }
 
     public function edit($ID)
-{
-    $empl = empleados::find($ID);
+    {
+        $empl = empleados::find($ID);
 
-    return view('/Equipo/formulario-editar') -> with('empleado', $empl);
+        return view('/Equipo/formulario-editar')->with('empleado', $empl);
 
-}
-
-
-public function update(Request $req, $ID)
-{
-    
-    $empld = empleados::find($ID);
-
-    $empld->Nombres    = $req->Nombres;
-    $empld->Apellidos  = $req->Apellidos;
-    $empld->Correo     = $req->Correo;
-    $empld->Contrasena = $req->Contrasena;
-    $empld->Rol        = $req->Rol;
-    $empld->Estado     = $req->Estado;
-
-    // Manejo especial para archivo
-    if ($req->hasFile('Imagen')) {
-        $path = $req->file('Imagen')->store('empleados', 'public');
-        $empld->Imagen = $path;
     }
 
-    $empld->save();
-//Hola lemus es negro
-    return redirect('/equipo/listado');
-}
+    public function update(Request $req, $ID)
+    {
 
-public function destroy($ID)
-{
+        $empld = empleados::find($ID);
 
- $empleados = empleados::find($ID);
- $empleados -> delete();
+        $empld->Nombres = $req->Nombres;
+        $empld->Apellidos = $req->Apellidos;
+        $empld->Correo = $req->Correo;
+        $empld->Contrasena = $req->Contrasena;
+        $empld->Rol = $req->Rol;
+        $empld->Estado = $req->Estado;
 
-return redirect('/equipo/listado');
+        // Manejo especial para archivo
+        if ($req->hasFile('Imagen')) {
+            $imagen = $req->file('Imagen');
+            $nuevo_nombre = 'Empleado_'.$empld->ID.'.jpg';
+            $ruta = $imagen->storeAs('Imagenes/Empleados', $nuevo_nombre, 'public');
+            $empld->Imagen = '/storage/'.$ruta;
+        }
 
+        $empld->save();
 
-}
+        return redirect('/equipo/listado');
+    }
 
+    public function destroy($ID)
+    {
+
+        $empleados = empleados::find($ID);
+        $empleados->delete();
+
+        return redirect('/equipo/listado');
+
+    }
 }
